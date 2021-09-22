643f15e95fb8c154dab99628e88478427f394b54
Fabien F <dakeyras.crypto@gmail.com>
2021-09-22 19:04:48 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Under construction...</header>
+      <script
+        type="text/javascript"
+        src="https://app.mailjet.com/statics/js/iframeResizer.min.js"
+      ></script>
+      <iframe
+        class="mj-w-res-iframe"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+        src="https://app.mailjet.com/widget/iframe/6iJy/KE7"
+        width="100%"
+      ></iframe>
     </div>
   );
 }
