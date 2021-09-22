7591cd2274b78a3e4022379b9d25a3d7f4bcaf37
Fabien F <dakeyras.crypto@gmail.com>
2021-09-22 22:37:05 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <script
-        type="text/javascript"
-        src="https://app.mailjet.com/statics/js/iframeResizer.min.js"
-      ></script>
       <iframe
-        class="mj-w-res-iframe"
-        frameborder="0"
+        title="gInformatique"
+        className="mj-w-res-iframe"
+        frameBorder="0"
         scrolling="no"
-        marginheight="0"
-        marginwidth="0"
+        marginHeight="0"
+        marginWidth="0"
         src="https://app.mailjet.com/widget/iframe/6iJy/KE7"
         width="100%"
+        height="400px"
+      ></iframe>
+
+      <iframe
+        title="gFinance"
+        className="mj-w-res-iframe"
+        frameBorder="0"
+        scrolling="no"
+        marginHeight="0"
+        marginWidth="0"
+        src="https://app.mailjet.com/widget/iframe/6iJy/KEF"
+        width="100%"
+        height="400px"
       ></iframe>
     </div>
   );
