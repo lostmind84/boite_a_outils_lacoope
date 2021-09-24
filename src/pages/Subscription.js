f50f2f317d76ab06c5d5211e0f035596ce8e0823
Fabien F <dakeyras.crypto@gmail.com>
2021-09-24 22:34:55 +0200

    update

--- src/pages/Subscription.js
+++ src/pages/Subscription.js
@@ -49,19 +49,94 @@ function SubscriptionPage() {
               </div>
             </Tab>
             <Tab eventKey="achat" title="Achats">
-              <div className="p-2">Under construction</div>
+              <div className="p-2">
+                <iframe
+                  title="gAchats"
+                  class="mj-w-res-iframe"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                  src="https://app.mailjet.com/widget/iframe/6iJy/KGx"
+                  width="100%"
+                  height="800px"
+                ></iframe>
+              </div>
             </Tab>
             <Tab eventKey="comext" title="Comm. externe">
-              <div className="p-2">Under construction</div>
+              <div className="p-2">
+                <iframe
+                  title="gComExt"
+                  class="mj-w-res-iframe"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                  src="https://app.mailjet.com/widget/iframe/6iJy/KGz"
+                  width="100%"
+                  height="800px"
+                ></iframe>
+              </div>
             </Tab>
             <Tab eventKey="gouvernance" title="Gouvernance">
-              <div className="p-2">Under construction</div>
+              <div className="p-2">
+                <iframe
+                  title="gGouvernance"
+                  class="mj-w-res-iframe"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                  src="https://app.mailjet.com/widget/iframe/6iJy/KGA"
+                  width="100%"
+                  height="800px"
+                ></iframe>
+              </div>
             </Tab>
             <Tab eventKey="implanation" title="Implantation">
-              <div className="p-2">Under construction</div>
+              <div className="p-2">
+                <iframe
+                  title="gImplantation"
+                  class="mj-w-res-iframe"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                  src="https://app.mailjet.com/widget/iframe/6iJy/KGC"
+                  width="100%"
+                  height="800px"
+                ></iframe>
+              </div>
             </Tab>
             <Tab eventKey="educpop" title="Educ. populaire">
-              <div className="p-2">Under construction</div>
+              <div className="p-2">
+                <iframe
+                  title="gEducPop"
+                  class="mj-w-res-iframe"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                  src="https://app.mailjet.com/widget/iframe/6iJy/KGE"
+                  width="100%"
+                  height="800px"
+                ></iframe>
+              </div>
+            </Tab>
+            <Tab eventKey="gdm" title="GDM">
+              <div className="p-2">
+                <iframe
+                  title="gGDM"
+                  class="mj-w-res-iframe"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                  src="https://app.mailjet.com/widget/iframe/6iJy/KGF"
+                  width="100%"
+                  height="800px"
+                ></iframe>
+              </div>
             </Tab>
           </Tabs>
         </Col>
